36e56bc3dbc9d6b3d415c971be8a555ca2b238ba
adamclear <3589@holbertonschool.com>
2022-08-30 21:54:07 +0000

    reworked

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,20 +5,11 @@
 def canUnlockAll(boxes):
     ''' Returns True if all boxes can be opened, otherwise False '''
     unlockedBoxes = [0]
-    obtainedNewKey = True
-    # Loop through boxes until loop where no more new keys found
-    while obtainedNewKey is True:
-        obtainedNewKey = False
-        x = 0
-        while x < len(boxes):
-            # Check if we have the key to this box
-            if x in unlockedBoxes:
-                for key in boxes[x]:
-                    # If a new key is found add to list and reset indicator
-                    if key not in unlockedBoxes:
-                        unlockedBoxes.append(key)
-                        obtainedNewKey = True
-            x += 1
+    # Loop through unlockedBoxes adding new keys as we go
+    for x in unlockedBoxes:
+        for key in boxes[x]:
+            if key not in unlockedBoxes:
+                unlockedBoxes.append(key)
 
     # Loop through unlocked boxes and if there are any missing numbers
     # between 0 and length of boxes, return false
