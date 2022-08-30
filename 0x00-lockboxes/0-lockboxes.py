4ade269c18a7ca9a0de8c1d5d18cd8aa5c5ad20f
adamclear <3589@holbertonschool.com>
2022-08-30 20:35:54 +0000

    file 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+''' This module attempts to solve the Lockboxes problem '''
+
+
+def canUnlockAll(boxes):
+    ''' Returns True if all boxes can be opened, otherwise False '''
+    unlockedBoxes = [0]
+    boxList = enumerate(boxes)
+    obtainedNewKey = True
+    openedAllBoxes = True
+    while obtainedNewKey is True:
+        unlockedBoxes.sort()
+        obtainedNewKey = False
+        x = 0
+        while x < len(boxes):
+            if x in unlockedBoxes:
+                for key in boxes[x]:
+                    if key not in unlockedBoxes:
+                        unlockedBoxes.append(key)
+                        obtainedNewKey = True
+            x += 1
+
+    for id, box in boxList:
+        if id not in unlockedBoxes:
+            openedAllBoxes = False
+
+    return openedAllBoxes
