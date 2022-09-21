e7d271f6c656ce018af0522d089ef20e85968f4a
adamclear <3589@holbertonschool.com>
2022-09-21 22:20:13 +0000

    updated with exit condition

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,8 +9,13 @@ def minOperations(n):
         return 0
     numOps = 0
     while n > 1:
-        for x in range(2, 6):
+        success = 0
+        for x in range(2, 10):
             if n % x == 0:
                 n = n / x
                 numOps += x
+                success = 1
+        if success == 0:
+            return 0
+
     return numOps
