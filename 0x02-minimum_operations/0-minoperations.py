3528a20a17386840fafcce87e7fd8a2b012ec720
adamclear <3589@holbertonschool.com>
2022-09-21 22:42:54 +0000

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,17 +5,14 @@
 def minOperations(n):
     ''' Calculates the fewest number of operations needed to result
     in exactly n characters in the file. '''
-    if n < 1 or n is None:
+    if n < 2 or n is None:
         return 0
     numOps = 0
     while n > 1:
-        success = 0
-        for x in range(2, 10):
+        max = n + 2
+        for x in range(2, max):
             if n % x == 0:
-                n = n / x
+                n = n // x
                 numOps += x
-                success = 1
-        if success == 0:
-            return numOps + n
 
     return numOps
