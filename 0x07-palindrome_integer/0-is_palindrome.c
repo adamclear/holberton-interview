4f95082fac8db31516ae078ace11f7006bad02f9
adamclear <3589@holbertonschool.com>
2022-10-24 20:47:39 +0000

    added directory, README, files for task

---
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - Determines if n is a palindrome.
+ * @n: The number to determine if palindrome.
+ *
+ * Return: 1 if palindrome, 0 if not.
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long int reversed = 0;
+	int remainder;
+	unsigned long int copy_n = n;
+
+	while (copy_n != 0)
+	{
+		remainder = copy_n % 10;
+		reversed = reversed * 10 + remainder;
+		copy_n /= 10;
+		printf("%lu\n", reversed);
+	}
+	printf("%lu\n", reversed);
+
+	if (n == reversed)
+		return (1);
+	return (0);
+}
