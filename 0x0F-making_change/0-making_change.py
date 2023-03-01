a225b019cbb706b37e760b955d588bf215b1d791
adamclear <3589@holbertonschool.com>
2023-03-01 17:37:55 +0000

    updated task and gitignore

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 '''
-Given a pile of coins of different values, determine the fewest number of 
+Given a pile of coins of different values, determine the fewest number of
 coins needed to meet a given amount total. Coin values can be assumed to be
 infinite in number.
 '''
+
 
 def makeChange(coins, total):
     '''
@@ -15,13 +16,20 @@ def makeChange(coins, total):
     if (total < 1):
         return 0
     changeMade = [0] * (total + 1)
+    coins.sort(reverse=True)
+    print(changeMade)
     for coin in coins:
+        print('coin = ', coin)
         for x in range(coin, total + 1):
             if (changeMade[x - coin]):
                 if (changeMade[x]):
-                    changeMade[x] = min(changeMade[x], changeMade[x - coin] + 1)
+                    print('changeMade[x]')
+                    changeMade[x] = min(changeMade[x],
+                                        changeMade[x - coin] + 1)
                 else:
+                    print('!changeMade[x]')
                     changeMade[x] = changeMade[x - coin] + 1
             elif (not x % coin):
                 changeMade[x] += 1
+        print(changeMade)
     return changeMade[total] if changeMade[total] else -1
